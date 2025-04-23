a01fc199d3bd593ca3cc1a887abc7295c3abdd26
hezihao <766560719@qq.com>
2025-04-23 15:51:40 +0800

    接雨水

--- leetcode/array/Jump.java
+++ leetcode/array/Jump.java
@@ -17,8 +17,24 @@ public class Jump {
      * @return 到达最后一个下标所需的最小跳跃次数
      */
     public int jump(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int jumps = 0; // 跳跃次数
+        int currentEnd = 0; // 当前跳跃的结束位置
+        int farthest = 0; // 当前跳跃能到达的最远位置
+        for (int i = 0; i < nums.length - 1; i++) {
+            // 更新当前跳跃能到达的最远位置
+            farthest = Math.max(farthest, i + nums[i]);
+            // 如果到达当前跳跃的结束位置，则需要增加跳跃次数
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest; // 更新当前跳跃的结束位置
+            }
+        }
+        return jumps;
     }
+
 
     public static void main(String[] args) {
         Jump jump = new Jump();
@@ -32,6 +48,12 @@ public class Jump {
         System.out.println("输入: nums = " + Arrays.toString(nums2));
         System.out.println("输出: " + jump.jump(nums2));
         System.out.println("预期: 2");
+        System.out.println();
+
+        int[] nums3 = { 5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5 };
+        System.out.println("输入: nums = " + Arrays.toString(nums3));
+        System.out.println("输出: " + jump.jump(nums3));
+        System.out.println("预期: 5");
         System.out.println();
     }
 }
---
+++ leetcode/array/Trap.java
@@ -0,0 +1,65 @@
+package leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 
+ * 示例 1:
+ * 输入: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ * 解释: 雨水可以在索引 2、3 和 8 处接住，共 6 个单位的雨水。
+ */
+public class Trap {
+
+    /**
+     * 计算接雨水的总量
+     * 
+     * @param height 整数数组，表示柱子的高度
+     * @return 接雨水的总量
+     */
+    public int trap(int[] height) {
+        int water = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int min_high = 0;
+        while (left < right) {
+            while (left < right && height[left] <= min_high) {
+                left++;
+            }
+            while (left < right && height[right] <= min_high) {
+                right--;
+            }
+            min_high = Math.min(height[left], height[right]);
+            for (int i = left + 1; i < right; i++) {
+                if (height[i] < min_high) {
+                    water += min_high - height[i];
+                    height[i] = min_high;
+                }
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        Trap trap = new Trap();
+        int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        System.out.println("输入: height = " + Arrays.toString(height));
+        System.out.println("输出: " + trap.trap(height));
+        System.out.println("预期: 6");
+        System.out.println();
+
+        int[] height2 = { 4, 2, 0, 3, 2, 5 };
+        System.out.println("输入: height = " + Arrays.toString(height2));
+        System.out.println("输出: " + trap.trap(height2));
+        System.out.println("预期: 9");
+        System.out.println();
+
+        int[] height3 = { 2, 0, 2 };
+        System.out.println("输入: height = " + Arrays.toString(height3));
+        System.out.println("输出: " + trap.trap(height3));
+        System.out.println("预期: 2");
+        System.out.println();
+    }
+}
