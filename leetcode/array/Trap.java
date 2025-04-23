eef72c51781fd5ed126c6cf50d6a178248e6ede1
hezihao <766560719@qq.com>
2025-04-23 17:41:54 +0800

    接雨水优化

--- leetcode/array/Trap.java
+++ leetcode/array/Trap.java
@@ -42,23 +42,47 @@ public class Trap {
         return water;
     }
 
+    public int trap2(int[] height) {
+        int water = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int min_high = 0;
+        while (left < right) {
+            min_high = Math.min(height[left], height[right]);
+            if (height[left] < height[right]) {
+                left++;
+                while (left < right && height[left] <= min_high) {
+                    water += min_high - height[left];
+                    left++;
+                }
+            } else {
+                right--;
+                while (left < right && height[right] <= min_high) {
+                    water += min_high - height[right];
+                    right--;
+                }
+            }
+        }
+        return water;
+    }
+
     public static void main(String[] args) {
         Trap trap = new Trap();
         int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
         System.out.println("输入: height = " + Arrays.toString(height));
-        System.out.println("输出: " + trap.trap(height));
+        System.out.println("输出: " + trap.trap2(height));
         System.out.println("预期: 6");
         System.out.println();
 
         int[] height2 = { 4, 2, 0, 3, 2, 5 };
         System.out.println("输入: height = " + Arrays.toString(height2));
-        System.out.println("输出: " + trap.trap(height2));
+        System.out.println("输出: " + trap.trap2(height2));
         System.out.println("预期: 9");
         System.out.println();
 
         int[] height3 = { 2, 0, 2 };
         System.out.println("输入: height = " + Arrays.toString(height3));
-        System.out.println("输出: " + trap.trap(height3));
+        System.out.println("输出: " + trap.trap2(height3));
         System.out.println("预期: 2");
         System.out.println();
     }
