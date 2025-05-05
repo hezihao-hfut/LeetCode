b69ff7025449ba7798ab77ad784f24d74a1b2d14
hezihao <766560719@qq.com>
2025-05-06 02:13:43 +0800

    无重复字符的最长子串

--- leetcode/string/LengthOfLongestSubstring.java
+++ leetcode/string/LengthOfLongestSubstring.java
@@ -1,5 +1,8 @@
 package leetcode.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * 3. 无重复字符的最长子串
  * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -7,8 +10,17 @@ package leetcode.string;
 public class LengthOfLongestSubstring {
     
     public int lengthOfLongestSubstring(String s) {
-        
-        return 0;
+        if (s == null || s.isEmpty()) return 0;
+        int max_len = 1;
+        int last_index = -1;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put(s.charAt(0), 0);
+        for (int i = 1; i < s.length(); i++) {
+            last_index = Math.max(map.getOrDefault(s.charAt(i), last_index), last_index);
+            map.put(s.charAt(i), i);
+            max_len = Math.max(max_len, i - last_index);
+        }
+        return max_len;
     }
 
     public static void main(String[] args) {
@@ -20,11 +32,11 @@ public class LengthOfLongestSubstring {
         System.out.println("预期: " + 3);
         System.out.println();
 
-        s = "bbbbb";
+        s = "abba";
         result = solution.lengthOfLongestSubstring(s);
         System.out.println("输入: s = " + s);
         System.out.println("输出: " + result);
-        System.out.println("预期: " + 1);
+        System.out.println("预期: " + 2);
         System.out.println();
 
         s = "pwwkew";
