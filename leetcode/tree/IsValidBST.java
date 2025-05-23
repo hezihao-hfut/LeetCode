e9bf18e30a118abb6108c30e01821a9e190e3cd8
hezihao <766560719@qq.com>
2025-05-23 16:33:16 +0800

    屏蔽测试

--- leetcode/tree/IsValidBST.java
+++ leetcode/tree/IsValidBST.java
@@ -3,6 +3,14 @@ package leetcode.tree;
 import java.util.Deque;
 import java.util.LinkedList;
 
+/**
+ * 98. 验证二叉搜索树
+ * 给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+ * 有效 二叉搜索树定义如下：
+ * 节点的左子树只包含 小于 当前节点的数。
+ * 节点的右子树只包含 大于 当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ */
 public class IsValidBST {
     /**
      * 递归法，判断每个节点是否在(min, max)范围内
